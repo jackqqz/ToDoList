233e4ece5395a3c9262279ff6527e2498d740dce
jAcKqQ1 <jwan0405@student.monash.edu>
2025-08-28 15:23:15 +0800

    Added DependencyInjection

---
+++ ToDoList.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Infrastructure.Data;
+
+namespace Todo.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration cfg)
+    {
+        // DbContext (SQLite)
+        services.AddDbContext<AppDbContext>(opt =>
+            opt.UseSqlite(cfg.GetConnectionString("Sqlite") ?? "Data Source=todo.db"));
+
+        return services;
+    }
+}
