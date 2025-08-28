8a568a55d927460a963aa051a6e67ccb02626ff7
jAcKqQ1 <jwan0405@student.monash.edu>
2025-08-28 15:23:36 +0800

    Created ToDoItem Entity

--- ToDoList.Core/Models/ToDoItem.cs
+++ ToDoList.Core/Models/ToDoItem.cs
@@ -8,6 +8,12 @@ namespace ToDoList.Core.Models
 {
     public class ToDoItem
     {
+        public Guid Id { get; set; }
+        public required String Title { get; set; }
+        public String? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; }
+
 
     }
 }
